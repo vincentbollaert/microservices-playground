56b69a21bd47a3bb7382bc51af8cafbbba2df289
vincent <vincentbollaert@gmail.com>
2020-08-26 15:06:43 +0700

    comment-mod post commentModerated event

--- comment-moderation/routes/index.js
+++ comment-moderation/routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
-const getModeration = (req, res) => {
-  console.log('GET MODERATION')
+const addEvent = async (req, res) => {
+  const { type, data } = req.body
+
+  if (type === 'commentCreated') {
+    console.log('moderate comment')
+    const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+    await axios.post('http://localhost:8080/events', {
+      type: 'commentModerated',
+      data: { ...data, status }
+    })
+  }
+
   res.send({})
 }
 
-router.route('/tester').get(getModeration);
+router.route('/events').post(addEvent);
 
 module.exports = router;
