dd62d975e5838ae208c88e3647fe1a80e4aeeb80
vincent <vincentbollaert@gmail.com>
2020-08-26 16:01:38 +0700

    event-bus store events

--- event-bus/routes/index.js
+++ event-bus/routes/index.js
@@ -2,14 +2,23 @@ var express = require('express');
 const axios = require('axios');
 var router = express.Router();
 
+const events = []
+
 const addEvent = (req, res) => {
   const event = req.body
+  events.push(event)
+
   axios.post('http://localhost:8000/events', event)
   axios.post('http://localhost:8001/events', event)
   axios.post('http://localhost:8002/events', event)
   axios.post('http://localhost:8010/events', event)
   res.send({ status: 'okay '})
 }
-router.route('/events').post(addEvent);
+
+const getEvents = (req, res) => {
+  res.send(events)
+}
+
+router.route('/events').post(addEvent).get(getEvents);
 
 module.exports = router;
