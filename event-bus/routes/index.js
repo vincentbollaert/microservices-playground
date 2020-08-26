1a52fa54a266efd06bee18753aac89fea18e7343
vincent <vincentbollaert@gmail.com>
2020-08-26 15:05:58 +0700

    event bus forward to comment-mod service

--- event-bus/routes/index.js
+++ event-bus/routes/index.js
@@ -6,9 +6,10 @@ const addEvent = (req, res) => {
   const event = req.body
   axios.post('http://localhost:8000/events', event)
   axios.post('http://localhost:8001/events', event)
+  axios.post('http://localhost:8002/events', event)
   axios.post('http://localhost:8010/events', event)
   res.send({ status: 'okay '})
 }
-router.route('/').post(addEvent);
+router.route('/events').post(addEvent);
 
 module.exports = router;
